0b5759d2005fca3fed0b2ced667841954ff8634c
mlambow <wandile.mlambo@yahoo.com>
2022-07-07 03:18:51 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+
+/**
+ *
+ * * print_alphabet -> prints the lowercase alphabets
+ *
+ * */
+
+
+
+void print_alphabet(void)
+
+{
+	int j;
+	for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+			_putchar('\n');
+
+}
