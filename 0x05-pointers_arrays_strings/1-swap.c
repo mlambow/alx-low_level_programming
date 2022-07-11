b81e35caba960ebe0db76a328935f06c22eaaac8
mlambow <wandile.mlambo@yahoo.com>
2022-07-11 14:49:12 -0700

    swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,12 +4,14 @@
  * swap_int - swaps the values of a and b
  * @a: pointer 1 
  * @b: pointer 2 
- * Return; 0
+ * Return; void
  */
 
-void swap_int(int *a, int #b)
+void swap_int(int *a, int *b)
 {
+
 	*a += *b;
 	*b = *a - *b;
 	*a = *a - *b;
+	
 }
