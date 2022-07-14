10f30d2fea7a7e12eca40cf98be3a753ead16caa
mlambow <wandile.mlambo@yahoo.com>
2022-07-14 02:29:22 -0700

    pas

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *_strncat - concatenate two strings but add inputted number of bytes
+ **_strncat - concatenate two strings but add inputted number
  *@dest: string to be appended upon
  *@src: string to be completed at end of dest
  *@n:integer parameter to compare index to
@@ -14,7 +14,9 @@ char *_strncat(char *dest, char *src, int n)
 	
 	while (dest[index++])
 		dest_len++;
+
 	for (index = 0; src[index] && index < n; index++)
 		dest[dest_len++] = src[index];
+
 	return (dest);
 }
