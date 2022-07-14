7668330d87b4f21df3b960a5f375d14edd368139
mlambow <wandile.mlambo@yahoo.com>
2022-07-14 02:40:55 -0700

    pas

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- *leet - encodes a string into 1337.
- *letters a and A are replaced by 4.
+ *leet - encodes a string into 1337
+ *letters a and A are replaced by 4
  *Letters e and E are replaced by 3
  *Letters o and O are replaced by 0
  *Letters t and T are replaced by 7
@@ -21,6 +21,7 @@ char *leet(char *s)
 	while (s[stringCount] != '\0')
 		{
 			leetCount = 0;
+
 			while (leetCount < 10)
 			{
 				if (leetLetters[leetCount] == s[stringCount])
