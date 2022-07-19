4962c1054c87affe58f953143fc53c1663f3e4c5
mlambow <wandile.mlambo@yahoo.com>
2022-07-19 06:30:17 -0700

    pas

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,9 +12,10 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < n; i++)
+	while (i < n)
 	{
-		dset[i] = src[i];
+		*(dest + i) = *(src + i);
+		i++;
 	}
 	return (dest);
 }
