f0eb4331422ee0e7cc294c62e253ac945cf533fe
mlambow <wandile.mlambo@yahoo.com>
2022-07-20 03:14:00 -0700

    recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _prints_rev_recursion - prints a string in reverse
+* @s: the string to be printed
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(char *s);
+		_putchar(*s);
+	}
+}
